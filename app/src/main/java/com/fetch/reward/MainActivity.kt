d688ecc2562d254ea932c70912f5637c6fb4c52b
Josue Marfil <josue7@users.noreply.github.com>
2023-09-03 01:16:59 -0600

    Binding y fragment
    Se agrego el bindign y se agrego funciones para abrir el fragment según el tab que elija el usuario

--- app/src/main/java/com/fetch/reward/MainActivity.kt
+++ app/src/main/java/com/fetch/reward/MainActivity.kt
@@ -2,10 +2,37 @@ package com.fetch.reward
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.fetch.reward.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    public lateinit var viewPagerAdapter: ViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setTab()
+    }
+
+    private fun setTab(){
+        val adapter = ViewPagerAdapter(this, binding.tabMenu.tabCount)
+        binding.vp2ListItem.adapter = adapter
+        binding.vp2ListItem.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                binding.tabMenu.selectTab(binding.tabMenu.getTabAt(position))
+            }
+        })
+
+        binding.tabMenu.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                binding.vp2ListItem.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) { }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) { }
+        })
     }
 }
