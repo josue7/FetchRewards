7f7b6457d97f578903bd1581c684e1006e1d96df
Josue Marfil <josue7@users.noreply.github.com>
2023-09-03 01:31:54 -0600

    Identificación de elementos de recycler
    Se identifico los elementos que se van a usar para el recyclerview

--- app/src/main/java/com/fetch/reward/viewholder/VHListItemHiring.kt
+++ app/src/main/java/com/fetch/reward/viewholder/VHListItemHiring.kt
@@ -3,7 +3,14 @@ package com.fetch.reward.viewholder
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.fetch.reward.databinding.ItemHiringBinding
+import java.lang.invoke.MethodType
 
 class VHListItemHiring(view: View): RecyclerView.ViewHolder(view) {
     private val binding = ItemHiringBinding.bind(view)
+
+    fun bind(item: MethodType){
+        binding.tvListID.text = "p"
+        binding.tvId.text = "i"
+        binding.tvName.text = "s"
+    }
 }
