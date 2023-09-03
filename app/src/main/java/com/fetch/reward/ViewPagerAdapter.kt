94a9cb328cc6a351a18bbb8d6057bfd1e63e93ef
Josue Marfil <josue7@users.noreply.github.com>
2023-09-03 01:12:37 -0600

    Agregación de fragment
    Se creo un fragment para mostrar la lista de datos en el TabLayout

---
+++ app/src/main/java/com/fetch/reward/ViewPagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.fetch.reward
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragment: FragmentActivity, private val tabCount: Int) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = tabCount
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position){
+            in 0..3 -> {
+                val bundle = Bundle()
+                val fragment = FragmentListID()
+                bundle.putString("text", "LisID${position+1}")
+                fragment.arguments = bundle
+                return fragment
+                //FragmentListID()
+            }
+            else -> FragmentListID()
+        }
+    }
+}
