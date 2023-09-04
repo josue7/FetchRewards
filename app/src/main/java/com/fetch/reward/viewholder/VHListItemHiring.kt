5b7f977e9f528115272d1117e1f8e6cc75dd5f80
Josue Marfil <josue7@users.noreply.github.com>
2023-09-03 18:16:10 -0600

    Cambio por valores
    Se agregaron las variables de donde se tomaran los valores que estran en el recyclerview

--- app/src/main/java/com/fetch/reward/viewholder/VHListItemHiring.kt
+++ app/src/main/java/com/fetch/reward/viewholder/VHListItemHiring.kt
@@ -3,13 +3,14 @@ package com.fetch.reward.viewholder
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.fetch.reward.databinding.ItemHiringBinding
+import com.fetch.reward.datos.DataHiringItem
 
 class VHListItemHiring(view: View): RecyclerView.ViewHolder(view) {
     private val binding = ItemHiringBinding.bind(view)
 
-    fun bind(item: String){
-        binding.tvListID.text = "p"
-        binding.tvId.text = "i"
-        binding.tvName.text = "s"
+    fun bind(item: DataHiringItem){
+        binding.tvListID.text = item.listId.toString()
+        binding.tvId.text = item.id.toString()
+        binding.tvName.text = item.name
     }
 }
