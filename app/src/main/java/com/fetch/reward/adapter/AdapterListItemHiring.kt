2960454cc097171a81e21de19a75a9095fe87220
Josue Marfil <josue7@users.noreply.github.com>
2023-09-03 18:14:31 -0600

    Cambio de tipo
    Se cambio el tipo de la lista por un DataHiringItem

--- app/src/main/java/com/fetch/reward/adapter/AdapterListItemHiring.kt
+++ app/src/main/java/com/fetch/reward/adapter/AdapterListItemHiring.kt
@@ -4,9 +4,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.fetch.reward.R
+import com.fetch.reward.datos.DataHiringItem
 import com.fetch.reward.viewholder.VHListItemHiring
 
-class AdapterListItemHiring(val data:List<String>): RecyclerView.Adapter<VHListItemHiring>() {
+class AdapterListItemHiring(private val data:List<DataHiringItem>): RecyclerView.Adapter<VHListItemHiring>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VHListItemHiring {
         val layoutInflater = LayoutInflater.from(parent.context)
         return VHListItemHiring(layoutInflater.inflate(R.layout.item_hiring, parent, false))
