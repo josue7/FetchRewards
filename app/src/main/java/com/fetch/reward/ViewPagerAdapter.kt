78d92b2a6271e67d94ecd18e1c0950c52c6ce9dc
Josue Marfil <josue7@users.noreply.github.com>
2023-09-03 19:30:24 -0600

    Quitar codigo
    Se elimino el código comentado que ya no e utiliza

--- app/src/main/java/com/fetch/reward/ViewPagerAdapter.kt
+++ app/src/main/java/com/fetch/reward/ViewPagerAdapter.kt
@@ -1,6 +1,7 @@
 package com.fetch.reward
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -8,20 +9,39 @@ import com.fetch.reward.datos.DataHiringItem
 
 class ViewPagerAdapter(fragment: FragmentActivity, private val tabCount: Int, private var itemHiring: List<DataHiringItem>) : FragmentStateAdapter(fragment) {
     private val orderHiring = ordeListHiring(itemHiring)
+
     override fun getItemCount(): Int = tabCount
 
     override fun createFragment(position: Int): Fragment {
         return when (position){
 
-            in 0..3 -> {
+            0 -> {
                 val fragment = FragmentListID()
-                //val list = mutableListOf<String>()
-                fragment.setListData(orderHiring)
-
-                //bundle.putString("text", "LisID${position+1}")
-                //fragment.arguments = bundle
+                val listFilter = filterByListId(orderHiring, 1)
+                Log.i("DATA FILTER1", listFilter.toString())
+                fragment.setListData(listFilter)
                 return fragment
-                //FragmentListID()
+            }
+            1 -> {
+                val fragment = FragmentListID()
+                val listFilter = filterByListId(orderHiring, 2)
+                Log.i("DATA FILTER2", listFilter.toString())
+                fragment.setListData(listFilter)
+                return fragment
+            }
+            2 -> {
+                val fragment = FragmentListID()
+                val listFilter = filterByListId(orderHiring, 3)
+                Log.i("DATA FILTER2", listFilter.toString())
+                fragment.setListData(listFilter)
+                return fragment
+            }
+            3-> {
+                val fragment = FragmentListID()
+                val listFilter = filterByListId(orderHiring, 4)
+                Log.i("DATA FILTER4", listFilter.toString())
+                fragment.setListData(listFilter)
+                return fragment
             }
             else -> FragmentListID()
         }
@@ -29,5 +49,8 @@ class ViewPagerAdapter(fragment: FragmentActivity, private val tabCount: Int, pr
 
     private fun ordeListHiring(dataHiring: List<DataHiringItem>): List<DataHiringItem> =
         dataHiring.sortedWith(compareBy ( {it.listId}, {it.name} ))
+
+    private fun filterByListId(dataHiring: List<DataHiringItem>, id: Int): List<DataHiringItem> =
+        dataHiring.filter { it.listId == id }
 
 }
