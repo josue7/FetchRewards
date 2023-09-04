fd7c5c36fd2fa7f51ed2e363c1a272830d46e385
Josue Marfil <josue7@users.noreply.github.com>
2023-09-03 18:12:56 -0600

    Lista de items hiring
    Se cambio los datos de ejemplo por la lista de items para mostrar en el recyclerview

--- app/src/main/java/com/fetch/reward/ViewPagerAdapter.kt
+++ app/src/main/java/com/fetch/reward/ViewPagerAdapter.kt
@@ -4,24 +4,30 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.fetch.reward.datos.DataHiringItem
 
-class ViewPagerAdapter(fragment: FragmentActivity, private val tabCount: Int) : FragmentStateAdapter(fragment) {
+class ViewPagerAdapter(fragment: FragmentActivity, private val tabCount: Int, private var itemHiring: List<DataHiringItem>) : FragmentStateAdapter(fragment) {
+    private val orderHiring = ordeListHiring(itemHiring)
     override fun getItemCount(): Int = tabCount
 
     override fun createFragment(position: Int): Fragment {
         return when (position){
-            in 0..3 -> {
-                val bundle = Bundle()
-                val fragment = FragmentListID()
-                val list = mutableListOf<String>()
 
-                list.add("uyiiuy")
-                bundle.putString("text", "LisID${position+1}")
-                fragment.arguments = bundle
+            in 0..3 -> {
+                val fragment = FragmentListID()
+                //val list = mutableListOf<String>()
+                fragment.setListData(orderHiring)
+
+                //bundle.putString("text", "LisID${position+1}")
+                //fragment.arguments = bundle
                 return fragment
                 //FragmentListID()
             }
             else -> FragmentListID()
         }
     }
+
+    private fun ordeListHiring(dataHiring: List<DataHiringItem>): List<DataHiringItem> =
+        dataHiring.sortedWith(compareBy ( {it.listId}, {it.name} ))
+
 }
